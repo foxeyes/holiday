87f07f29641677295d7c87b02383cc71a25a0472
alex.sova <foxrandom.com@gmail.com>
2019-06-12 12:20:22 +0300

    holiday.js exports: HdGraph excluded

--- core/holiday.js
+++ core/holiday.js
@@ -1,4 +1,6 @@
 export { HdElement } from './hd-element.js';
 export { HdRouter } from './hd-router.js';
 export { HdState } from './hd-state.js';
-export { HdGraph } from './hd-graph.js';
+
+// HdGraph module can be imported separately when needed:
+// export { HdGraph } from './hd-graph.js';
