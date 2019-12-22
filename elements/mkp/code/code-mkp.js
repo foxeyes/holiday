eba7f588b17dfc81f8b5e7e8b38cf6ebc67578c2
alex.sova <foxrandom.com@gmail.com>
2019-12-22 18:53:31 +0300

    gist-embed & code-mkp: syntax highlighting added

--- elements/mkp/code/code-mkp.js
+++ elements/mkp/code/code-mkp.js
@@ -1,6 +1,52 @@
 import { HdElement } from '../../../core/hd-element.js';
 
-class CodeMkp extends HdElement {}
+class CodeMkp extends HdElement {
+  colorize() {
+    if (!this.hasAttribute('highlight')) {
+      return;
+    }
+    let html = this.innerHTML;
+    let hlChars = [
+      '{',
+      '}',
+      `'`,
+      '`',
+      `:`,
+      `.`,
+      `,`,
+      `(`,
+      `)`,
+      `[`,
+      `]`,
+    ];
+    hlChars.forEach((char) => {
+      html = html.split(char).join(`<span class="code-mkp-hl">${char}</span>`)
+    });
+
+    html = html.split('/*').map((subStr) => {
+      return subStr.replace('*/', '*/</span>');
+    }).join('<span class="code-mkp-comment">/*');
+
+    html = html.split('//').map((subStr) => {
+      return subStr.replace('\n', '</span>\n');
+    }).join('<span class="code-mkp-comment">//');
+
+    this.innerHTML = html;
+  }
+
+  constructor() {
+    super();
+    this.defineAccessor('highlight', (color) => {
+      this.style.setProperty('--hl-color', color);
+    });
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.colorize();
+  }
+
+}
 
 CodeMkp.template = /*html*/ `
 <style>
@@ -24,9 +70,23 @@ CodeMkp.template = /*html*/ `
     opacity: 0.05;
     pointer-events: none;
   }
+  ::slotted(.code-mkp-hl) {
+    color: var(--hl-color);
+  }
+  ::slotted(.code-mkp-comment) {
+    color: var(--hl-color);
+    font-style: italic;
+    opacity: 0.7;
+  }
+  code {
+    white-space: pre-wrap;
+  }
 </style>
 <pre><code><slot></slot></code></pre>
 `;
+CodeMkp.logicAttributes = [
+  'highlight',
+];
 CodeMkp.is = 'code-mkp';
 
 export { CodeMkp };
